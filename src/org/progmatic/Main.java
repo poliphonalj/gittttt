7e82668628441fa3b3af06b834cf9fc29d597b1c
aronjt <jtelekiaron@gmail.com>
2020-11-10 16:26:02 +0100

    reader

--- src/org/progmatic/Main.java
+++ src/org/progmatic/Main.java
@@ -2,14 +2,17 @@ package org.progmatic;
 
 //import java.io.Reader;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Reader people = new Reader();
+    public static void main(String[] args) throws FileNotFoundException {
+        Reader.read();
+        System.out.println();
+
 
     }
 
