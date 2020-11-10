b435e78b4fcc6f5df6324179ecaccaba98dc2326
peteri gabor <peteri@t-online.hu>
2020-11-10 15:48:07 +0100

    kezdem a Person Classt

---
+++ src/org/progmatic/Person.java
@@ -0,0 +1,21 @@
+package org.progmatic;
+
+public class Person {
+
+
+        private String firstName;
+        private String lastName;
+        private String companyName;
+        private String address;
+        private String city;
+        private String county;
+        private String state;
+        private int zip;
+        private String phone1;
+        private String phone2;
+        private String email;
+        private String web;
+
+
+
+}
