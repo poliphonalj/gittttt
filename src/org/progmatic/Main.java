cbbd8432abe8073fd4e44fdf2aabd3c3cfba655e
aronjt <jtelekiaron@gmail.com>
2020-11-10 16:13:48 +0100

    fixed some stuff

--- src/org/progmatic/Main.java
+++ src/org/progmatic/Main.java
@@ -2,18 +2,19 @@ package org.progmatic;
 
 //import java.io.Reader;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList <Person> personContainer=Reader.read();
+        Reader people = new Reader();
 
     }
 
-    public static ArrayList<Person>sortArray(ArrayList<Person>lista){
-        Collections.sort(lista);
+    public static ArrayList<Person> sortArray(ArrayList<Person>lista){
+       // Collections.sort(lista);
         return lista;
     }
 
