91825dabe6b024fbf67b187c8af0aa95bd9b6c79
peteri gabor <peteri@t-online.hu>
2020-11-10 21:28:41 +0100

    comparatorral bovitettem es a Filter osztaly megirva

--- src/org/progmatic/Filter.java
+++ src/org/progmatic/Filter.java
@@ -1,4 +1,17 @@
 package org.progmatic;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Filter {
+//Írj egy Filter class-t, és annak írj egy cityFilter(String city, List<Person>) statikus metódust!
+
+    public static ArrayList<Person> cityFilter(String city, ArrayList<Person> people){
+        ArrayList <Person>actualPeople=new ArrayList<>();
+        for (Person actualPerson : people) {
+            if(city.equals(actualPerson.getCity())) {
+                actualPeople.add(actualPerson);
+            }
+        }
+        return actualPeople;
+    }
 }
